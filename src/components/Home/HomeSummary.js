8d544c3932787ea401afe46dd917f502ef5afc68
Kacper Dufrat <kacper@dufrat.pl>
2020-03-10 12:25:11 +0100

    Working pagination

--- src/components/Home/HomeSummary.js
+++ src/components/Home/HomeSummary.js
@@ -1,69 +1,19 @@
 import React, {useState} from "react";
-import Foundations from "./Pagination/Foundations.js";
-import Locals from "./Pagination/Locals.js";
-import Organizations from "./Pagination/Organizations.js";
 import {ReactComponent as Decoration} from "../../assets/Decoration.svg";
-import "../../scss/HomeSummary.scss";
 import Posts from "./Pagination/Posts.js";
+import "../../scss/HomeSummary.scss";
+
 
 const HomeSummary = () => {
-    const [type, setType] = useState("foundation");
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(3);
 
-    const handleType = (name) => {
-        setType(name);
-    }
-
-    let content;
-
-    if (type === "organizations") {
-        content = (
-            <>
-            <h1>Organizacja xxx</h1>
-            <p>opis organizacji xxx</p>
-            <ul>
-                {Organizations.map(el => <li>{el.title}, {el.description}, {el.info}</li>)}
-            </ul>
-            </>
-        )
-    } else if (type === "locals") {
-        content = (
-            <>
-            <h1>Lokalna zbiórka xxx</h1>
-            <p>opis lokalnej zbiórki xxx</p>
-            <ul>
-                {Locals.map(el => <li>{el.title}, {el.description}, {el.info}</li>)}
-            </ul>
-            </>
-        )
-    } else {
-        content = (
-            <>
-            <h1>Fundacja xxx</h1>
-            <p>opis fundacji xxx</p>
-            <ul>
-                {Foundations.map(el => <li>{el.title}, {el.description}, {el.info}</li>)}
-            </ul>
-            </>
-        )
-    }
-
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = Foundations.slice(indexOfFirstPost, indexOfLastPost);
 
     return (
     <section className="summary">
         <h2>Komu pomagamy?</h2>
         <Decoration className="summary-decoration"/>
-        <div className="summary-btn">
-            <button onClick={() => handleType("foundations")}>Fundacjom</button>
-            <button onClick={() => handleType("organizations")}>Organizacjom pozarządowym</button>
-            <button onClick={() => handleType("locals")}>Lokalnym zbiórkom</button>
-        </div>
+
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        {content}
+        <Posts />
     </section>
     )
 }
--- src/components/Home/Pagination/Posts.js
+++ src/components/Home/Pagination/Posts.js
@@ -1,10 +1,81 @@
-import React from "react";
+import React, {useState} from "react";
+import Foundations from "./Foundations.js";
+import Locals from "./Locals.js";
+import Organizations from "./Organizations.js";
+import Pagination from "./Pagination.js";
 
-const Posts = ({posts}) => {
+const Posts = () => {
+    const [type, setType] = useState("foundation");
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(3);
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const organizationsPosts = Organizations.slice(indexOfFirstPost, indexOfLastPost);
+    const foundationsPosts = Foundations.slice(indexOfFirstPost, indexOfLastPost);
+    const localsPosts = Locals.slice(indexOfFirstPost, indexOfLastPost);
+
+ 
+
+    let totalPosts;
+    let content;
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    }
+    
+    const handleType = (name) => {
+        setType(name);
+    }
+
+
+    if (type === "organizations") {
+        totalPosts = Organizations.length;
+        content = (
+            <>
+            <h1>Organizacja xxx</h1>
+            <p>opis organizacji xxx</p>
+            <ul>
+                {organizationsPosts.map(el => <li>{el.title}, {el.description}, {el.info}</li>)}
+            </ul>
+            </>
+        )
+    } else if (type === "locals") {
+        totalPosts = Locals.length;
+        content = (
+            <>
+            <h1>Lokalna zbiórka xxx</h1>
+            <p>opis lokalnej zbiórki xxx</p>
+            <ul>
+                {localsPosts.map(el => <li>{el.title}, {el.description}, {el.info}</li>)}
+            </ul>
+            </>
+        )
+    } else {
+        totalPosts = Foundations.length;
+        content = (
+            <>
+            <h1>Fundacja xxx</h1>
+            <p>opis fundacji xxx</p>
+            <ul>
+                {foundationsPosts.map(el => <li>{el.title}, {el.description}, {el.info}</li>)}
+            </ul>
+            </>
+        )
+    }
+
+
     return (
-        <ul>
-            ({posts.map(post => <li>{post.title}</li>)})
-        </ul>
+        <>
+        <div className="summary-btn">
+            <button onClick={() => handleType("foundations")}>Fundacjom</button>
+            <button onClick={() => handleType("organizations")}>Organizacjom pozarządowym</button>
+            <button onClick={() => handleType("locals")}>Lokalnym zbiórkom</button>
+    `   </div>
+        <div>{content}</div>
+
+        <Pagination postsPerPage={postsPerPage} totalPosts={totalPosts} paginate={paginate}/>
+        </>
     )
 }
 
